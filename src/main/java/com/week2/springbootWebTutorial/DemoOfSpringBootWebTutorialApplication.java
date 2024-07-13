e403929e17a0b5486d0b5010874763fcb5384152
shaikmunsif <shaikmunsif4@gmail.com>
2024-07-13 12:42:25 +0530

    week2 practice1

---
+++ src/main/java/com/week2/springbootWebTutorial/DemoOfSpringBootWebTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.week2.springbootWebTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOfSpringBootWebTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOfSpringBootWebTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/week2/springbootWebTutorial/DemoOfSpringBootWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week2.springbootWebTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOfSpringBootWebTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
